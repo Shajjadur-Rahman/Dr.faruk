a9f93562b8d5ddbded4b8d15bf52d51b9011fc8d
Hamza Zahidul Islam <56122568+hamzazahidulislam@users.noreply.github.com>
2020-12-05 20:36:31 +0300

    Update settings.py

--- Shop_management/settings.py
+++ Shop_management/settings.py
@@ -150,7 +150,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 LOGIN_URL = '/account/login'
